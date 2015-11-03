9e5ed22ca93061b1803e322d24d8018e202940e5
root <marian.preda0809@gmail.com>
2015-11-03 19:48:33 +0200

    second commit

---
+++ champ1/alo.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+	int E, N, Nr, D;
+	FILE *fp_in, *fp_out;
+	fp_in = fopen("alo.in", r);
+	E = fscanf(fp_in, "%d", &E);
+	N = fscanf(fp_in, "%d", &N);
+
+	while (getchar() != EOF) {
+		Nr = fscanf(fp_in, "%d", &Nr);
+		D = fscanf(fp_in, "%d", &D);
+
+		if (Nr % 10 == 5 && Nr/1000 == 2) {
+			E += D;
+		}
+
+
+
+
+
+
+	}
+
+
+}
