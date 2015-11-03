13a4aa8a37d1bff86d63890601df6cacddd4ca91
root <marian.preda0809@gmail.com>
2015-11-03 22:25:29 +0200

    up

--- champ1/alo.c
+++ champ1/alo.c
@@ -1,31 +1,23 @@
 #include <stdio.h>
 
 int main() {
-	int E, N, Nr, D;
+	int i, E, N, Nr, D;
 	FILE *fp_in, *fp_out;
-	fp_in = fopen("alo.in", r);
+	fp_in = fopen("alo.in", "r");
 	E = fscanf(fp_in, "%d", &E);
 	N = fscanf(fp_in, "%d", &N);
 
-	while (getchar() != EOF) {
+//	for (i = 0; i < N; i++) {
 		Nr = fscanf(fp_in, "%d", &Nr);
 		D = fscanf(fp_in, "%d", &D);
-
+		printf("%d", Nr);
+		printf("%d", D);
 		if (Nr % 10 == 5 && Nr/1000 == 2) {
 			E += D;
 		}
 		if (Nr % 10 == 5 && Nr/1000 == 1) {
 			E -= 2*D;
 		}
-
-
-
-
-
-
-
-
-	}
-
-
+//	}
+	printf("%d\n", E);
 }
