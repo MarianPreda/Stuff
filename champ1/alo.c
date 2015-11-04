643045ceb19c88320f19527ddcacd1292a84cc2e
root <marian.preda0809@gmail.com>
2015-11-04 14:51:00 +0200

    finally

--- champ1/alo.c
+++ champ1/alo.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int main() {
 	int i, E, N;
-	int Nr[10], D[10];
+	int *Nr, *D;
 	FILE *fp_in, *fp_out;
 	fp_in = fopen ("alo.in", "r");
 	fp_out = fopen ("alo.out", "w");
 
 	fscanf (fp_in, "%d", &E);
 	fscanf (fp_in, "%d", &N);
-
+	Nr = malloc(N * sizeof(int));
+	D = malloc(N * sizeof(int));
 	for (i = 0; i < N; ++i) {
 
-		scanf ("%d", &Nr[i]);
-		scanf ("%d", &D[i]);
+		fscanf (fp_in, "%d", &Nr[i]);
+		fscanf (fp_in, "%d", &D[i]);
 
 	}
 	for (i = 0; i < N; ++i) {
@@ -28,5 +29,6 @@ int main() {
 
 	fclose(fp_in);
 	fclose(fp_out);
+
 	return 0;
 }
