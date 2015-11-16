e647c10d51f4b442416ca30fd948186fc1962bc2
root <marian.preda0809@gmail.com>
2015-11-16 23:34:00 +0200

    try1

--- champ1/robinson.c
+++ champ1/robinson.c
@@ -6,13 +6,13 @@ int main () {
 	fp_in = fopen ("robinson.in", "r");
 	fp_out = fopen ("robinson.out", "w");
 
-	int i, j, m, n, l, c, dir, *v;
+	int i, j, k = 0, m, n, l, c, dir, *v;
 	fscanf (fp_in, "%d", &m);
 	fscanf (fp_in, "%d", &n);
 	fscanf (fp_in, "%d", &l);
 	fscanf (fp_in, "%d", &c);
 	int matrix[m][m];
-	v = malloc (m * m * sizeof(int));
+	v = malloc (2 * m * sizeof(int));
 
 	for (j = 1; j <= m; ++j) {
 		matrix[1][j] = n + j -1;
@@ -30,7 +30,39 @@ int main () {
 			}
 		}
 	}
+	i = l;
+	j = c;
+	int count = 0;
+	while (i > 0 || i < m || j > 0 || j > m) {
+		if (matrix[i][j] == 0)
+			break;
+		v[k] = i;
+		v[k + 1] = j;
+		k += 3;
+		count += 2;
+		if (matrix[i][j] % 4 == 0) {
+			matrix[i][j] = 0;
+			i++;
+		} else
+			if (matrix[i][j] % 4 == 1) {
+				matrix[i][j] = 0;
+				j++;
+			} else
+				if (matrix[i][j] % 4 == 2) {
+					matrix[i][j] = 0;
+					i--;
+				} else
+					if (matrix[i][j] % 4 == 3) {
+						matrix[i][j] = 0;
+						j--;
+					}
+	}
 
 	fprintf (fp_out, "%d\n", matrix[m][m]);
+	for (i = 0; i < count - 1; i++) {
+		fprintf (fp_out, "%d %d\n", v[i], v[i + 1]);
+	}
+	fclose(fp_in);
+	fclose(fp_out);
 	return 0;
 }
