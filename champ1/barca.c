eace5aee41a8099b3d50b85078258653f7b1a101
root <marian.preda0809@gmail.com>
2016-03-28 21:20:15 +0300

    empty message

---
+++ champ1/barca.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+	FILE *fp_in;
+	FILE *fp_out;
+	int n, i;
+
+	fp_in = fopen ("barca.in", "r");
+	fp_out = fopen ("barca.out", "w");
+
+	fscanf (fp_in, "%d", &n);
+
+	fprintf (fp_out, "%d\n", 4 * n);
+
+	for (i = 1; i <= n; ++i) {
+		fprintf (fp_out, "IG\n");
+		fprintf (fp_out, "I\n");
+		fprintf (fp_out, "%d\n", i);
+		fprintf (fp_out, "G%c", i == n ? '\0' :'\n');
+	}
+
+	fclose (fp_in);
+	fclose (fp_out);
+
+	return 0;
+}
